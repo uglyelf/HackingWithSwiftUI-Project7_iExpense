eb7f23be0c28b264e1551f798d8f0f3f4889ce10
Gregory Randolph <gloaming@gmail.com>
2025-08-01 19:29:10 -0700

    before navigation challenge changes

---
+++ iExpense/AddExpenseView.swift
@@ -0,0 +1,47 @@
+//
+//  AddExpenseView.swift
+//  iExpense
+//
+//  Created by Gregory Randolph on 6/30/25.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    @State private var name = ""
+    @State private var type = ExpenseType.personal
+    @State private var amount = 0.0
+    
+    @Environment(\.dismiss) var dismiss
+    
+    var expenses: Expenses
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(ExpenseType.allCases) { type in
+                        Text(type.rawValue)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "USD"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddExpenseView(expenses: Expenses())
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,16 +6,117 @@
 //
 
 import SwiftUI
+import Observation
+
 
 struct ContentView: View {
+    @State private var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                Section {
+                    ForEach(expenses.myExpenses) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type.rawValue)
+                            }
+                            
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .foregroundColor(item.amount <= 10 ? .green : (item.amount <= 100 ? .yellow : .red))
+                        }
+                    }
+                    .onDelete(perform: removePersonalExpense)
+                }
+                Section {
+                    ForEach(expenses.workExpenses) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                                Text(item.type.rawValue)
+                            }
+                            
+                            Spacer()
+                            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                                .foregroundColor(item.amount <= 10 ? .green : (item.amount <= 100 ? .yellow : .red))
+                        }
+                    }
+                    .onDelete(perform: removeBusinessExpense)
+                }
+                
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                   showingAddExpense = true
+                }
+            }
         }
-        .padding()
+        .sheet(isPresented: $showingAddExpense) {
+            AddExpenseView(expenses: expenses)
+        }
+    }
+
+    func removePersonalExpense(at offsets: IndexSet) {
+        for offset in offsets {
+            let id: UUID = expenses.myExpenses[offset].id
+            if let index = expenses.items.firstIndex(where: {$0.id == id}) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+    func removeBusinessExpense(at offsets: IndexSet) {
+        for offset in offsets {
+            let id: UUID = expenses.workExpenses[offset].id
+            if let index = expenses.items.firstIndex(where: {$0.id == id}) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+}
+
+enum ExpenseType: String, CaseIterable, Identifiable, Codable {
+    var id: Self { self }
+    case business, personal
+}
+
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    var date = Date.now
+    let name: String
+    let type: ExpenseType
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    var myExpenses: [ExpenseItem] {
+        return items.filter {$0.type == .personal}
+    }
+    var workExpenses: [ExpenseItem] {
+        return items.filter {$0.type == .business}
+    }
+    
+    init() {
+        guard let savedItems = UserDefaults.standard.data(forKey: "Items"),
+              let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) else {
+            items = []
+            return
+        }
+        items = decodedItems
     }
 }
 
